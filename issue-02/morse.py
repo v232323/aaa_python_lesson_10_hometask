b4d3a784b39ccfb0bfa96b054f6e1135e8edb4c4
v232323 <90322948+v232323@users.noreply.github.com>
2021-11-30 13:25:59 +0300

    Add files via upload

---
+++ issue-02/morse.py
@@ -0,0 +1,72 @@
+"""Morse Code Translator"""
+import pytest
+
+LETTER_TO_MORSE = {
+    'A': '.-', 'B': '-...', 'C': '-.-.',
+    'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..',
+    'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-',
+    'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '1': '.----',
+    '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', '0': '-----',
+    ', ': '--..--', '.': '.-.-.-', '?': '..--..',
+    '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-',
+    ' ': ' '
+}
+
+MORSE_TO_LETTER = {
+    morse: letter
+    for letter, morse in LETTER_TO_MORSE.items()
+}
+
+
+def encode(message: str) -> str:
+    """
+    Кодирует строку в соответсвие с таблицей азбуки Морзе
+    >>> encode('SOS')
+    '... --- ...'
+    >>> encode('AAAAA') #doctest: +ELLIPSIS
+    '.-....-'
+    >>> encode(5) #doctest: +ELLIPSIS
+    Traceback (most recent call last):
+        ...
+    TypeError: 'int' object is not iterable
+    """
+    encoded_signs = [
+        LETTER_TO_MORSE[letter] for letter in message
+    ]
+
+    return ' '.join(encoded_signs)
+
+
+def decode(morse_message: str) -> str:
+    """
+    Декодирует строку из азбуки Морзе в английский
+    >>> decode('... --- ...')
+    'SOS'
+    """
+    decoded_letters = [
+        MORSE_TO_LETTER[letter] for letter in morse_message.split()
+    ]
+
+    return ''.join(decoded_letters)
+
+@pytest.mark.parametrize('s,exp', [
+    ('... --- ...', 'SOS'),
+    ('.- .- .-','AAA'),
+    ('-... -... -...','BBB')
+])
+def test_decode(s: str, exp: str):
+    assert decode(s) == exp
+
+
+if __name__ == '__main__':
+    morse_msg = '-- .- .. -....- .--. -.-- - .... --- -. -....- ..--- ----- .---- ----.'
+    decoded_msg = decode(morse_msg)
+    print(decoded_msg)
+    assert morse_msg == encode(decoded_msg)
